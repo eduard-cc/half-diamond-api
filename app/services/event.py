a6c71258678d48327557ed8be20f4606d8dc1885
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-07 00:18:56 +0200

    simplify Event type and add to_dict method

--- app/services/event.py
+++ app/services/event.py
@@ -15,32 +15,8 @@ class Event:
         self.type = type
         self.data = data
 
-
-class NewHostEvent(Event):
-    def __init__(self, host: Host):
-        super().__init__(EventType.HOST_NEW, host)
-
-class HostSeenEvent(Event):
-    def __init__(self, host: Host):
-        super().__init__(EventType.HOST_SEEN, host)
-
-class HostConnectedEvent(Event):
-    def __init__(self, host: Host):
-        super().__init__(EventType.HOST_CONNECTED, host)
-
-class HostDisconnectedEvent(Event):
-    def __init__(self, host: Host):
-        super().__init__(EventType.HOST_DISCONNECTED, host)
-
-
-class TcpScanEvent(Event):
-    def __init__(self, host: Host):
-        super().__init__(EventType.SCAN_TCP, host)
-
-class SynScanEvent(Event):
-    def __init__(self, host: Host):
-        super().__init__(EventType.SCAN_SYN, host)
-
-class UdpScanEvent(Event):
-    def __init__(self, host: Host):
-        super().__init__(EventType.SCAN_UDP, host)
+    def to_dict(self):
+        return {
+            "type": self.type.value,
+            "data": self.data.model_dump()
+        }
