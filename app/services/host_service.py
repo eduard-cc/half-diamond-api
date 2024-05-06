6238e4905cc5623e97a32cf75e32fc6586f79746
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-06 23:12:07 +0200

    add update_ports func to update ports in cache and dispatch scan events

--- app/services/host_service.py
+++ app/services/host_service.py
@@ -1,9 +1,10 @@
 import asyncio
 from datetime import datetime
 from typing import Dict, List
-from services.event import NewHostEvent, HostConnectedEvent, HostDisconnectedEvent, HostLastSeenUpdatedEvent
+from services.port_scan_type import PortScanType
+from services.event import NewHostEvent, HostConnectedEvent, HostDisconnectedEvent, HostSeenEvent, SynScanEvent, TcpScanEvent, UdpScanEvent
 from services.event_handler import EventHandler
-from models.host import Host, Status
+from models.host import Host, Port, Status
 
 class HostService:
     def __init__(self, event_handler: EventHandler):
@@ -25,18 +26,36 @@ class HostService:
     def update_host(self, host: Host) -> None:
         existing_host = self.hosts[host.mac]
         existing_host.last_seen = datetime.now()
+
         if existing_host.status == Status.Offline:
             existing_host.status = Status.Online
             event = HostConnectedEvent(host)
             asyncio.run(self.event_handler.dispatch(event))
         else:
-            event = HostLastSeenUpdatedEvent(host)
+            event = HostSeenEvent(host)
             asyncio.run(self.event_handler.dispatch(event))
 
     def add_host(self, host: Host) -> None:
         self.hosts[host.mac] = host
         event = NewHostEvent(host)
         asyncio.run(self.event_handler.dispatch(event))
+
+    def update_ports(self, ports_by_ip: Dict[str, List[Port]], scan_type: PortScanType) -> None:
+        ip_to_host = {host.ip: host for host in self.hosts.values()}
+
+        for ip, ports in ports_by_ip.items():
+            target_host = ip_to_host.get(ip)
+            if target_host:
+                target_host.open_ports = ports
+
+                if scan_type == PortScanType.SYN:
+                    event = SynScanEvent(target_host)
+                elif scan_type == PortScanType.TCP:
+                    event = TcpScanEvent(target_host)
+                elif scan_type == PortScanType.UDP:
+                    event = UdpScanEvent(target_host)
+
+                asyncio.run(self.event_handler.dispatch(event))
 
     def check_and_update_offline_hosts(self) -> None:
         for host in self.hosts.values():
