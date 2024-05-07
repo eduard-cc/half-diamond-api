e03f6139dd55b520834ee5a97f2b71cdbde59823
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-07 18:43:16 +0200

    import session

--- app/routers/os_detection_router.py
+++ app/routers/os_detection_router.py
@@ -1,10 +1,11 @@
 from typing import Dict, List
 from fastapi import APIRouter
 from services.os_detection import OSDetection
+from services.session import session
 
 os_detection_router = APIRouter()
-os_detector = OSDetection()
 
 @os_detection_router.patch("/os", response_model=Dict[str, str])
 def scan_os(target_ips: List[str]):
+    os_detector = OSDetection(session.host_service)
     return os_detector.detect_os(target_ips)
