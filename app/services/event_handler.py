068af25ece70b4816df8f2c99001fd1ae959d302
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-15 16:59:58 +0200

    refactor EventHandler to store events and add get_events method

--- app/services/event_handler.py
+++ app/services/event_handler.py
@@ -1,24 +1,21 @@
 import logging
 import json
-from typing import Callable, Dict, List, Type
+from typing import List
 from fastapi import WebSocket
 from utils.date_time_encoder import DateTimeEncoder
 from services.event import Event
 
 class EventHandler:
     def __init__(self, websocket: WebSocket | None = None):
-        self.handlers: Dict[Type[Event], List[Callable[[Event], None]]] = {}
         self.websocket: WebSocket | None = websocket
-
-    def register(self, event_class: Type[Event], handler: Callable[[Event], None]):
-        if event_class not in self.handlers:
-            self.handlers[event_class] = []
-        self.handlers[event_class].append(handler)
+        self.events: List[Event] = []
 
     async def dispatch(self, event: Event):
-        logging.info(f"Dispatching event: {event.type} for host: {event.data.mac}")
-        for handler in self.handlers.get(type(event), []):
-            await handler(event)
+        self.events.append(event)
         if self.websocket:
+            logging.info(f"Dispatching event: {event.type} for host: {event.data.mac}")
             data = json.dumps(event.to_dict(), cls=DateTimeEncoder)
             await self.websocket.send_text(data)
+
+    def get_events(self) -> List[Event]:
+        return self.events
