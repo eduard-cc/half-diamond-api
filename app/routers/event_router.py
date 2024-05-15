0e40c6515a65736094092796d32fb06dab8d766f
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-15 17:29:47 +0200

    use websocket decorator

--- app/routers/event_router.py
+++ app/routers/event_router.py
@@ -5,6 +5,7 @@ from services.session import session
 
 event_router = APIRouter()
 
+@event_router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     try:
         await websocket.accept()
@@ -15,8 +16,6 @@ async def websocket_endpoint(websocket: WebSocket):
         session.event_handler.websocket = None
     except Exception:
         await websocket.close()
-
-event_router.add_route("/ws", websocket_endpoint, methods=["GET"], include_in_schema=False)
 
 @event_router.get("/", response_model=List[Event])
 def get_hosts():
