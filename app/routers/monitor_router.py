fb751d85dcbd332adc149134e87cd16596908d84
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-15 16:59:27 +0200

    move unused websocket route to event router

--- app/routers/monitor_router.py
+++ app/routers/monitor_router.py
@@ -1,22 +1,9 @@
-from fastapi import APIRouter, HTTPException, WebSocket, BackgroundTasks, WebSocketDisconnect
+from fastapi import APIRouter, HTTPException, BackgroundTasks
 from models.host import Host
 from typing import List
 from services.session import session
 
 monitor_router = APIRouter()
-
-@monitor_router.websocket_route("/ws")
-async def websocket_endpoint_monitor(websocket: WebSocket):
-    try:
-        await websocket.accept()
-        session.event_handler.websocket = websocket
-        while True:
-            await websocket.receive_text()
-    except WebSocketDisconnect as e:
-        print(f"WebSocket disconnected: {e}")
-        session.event_handler.websocket = None
-    except Exception as e:
-        await websocket.close()
 
 @monitor_router.get("/hosts", response_model=List[Host])
 def get_hosts():
