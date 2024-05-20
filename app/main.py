53465da6988d44d0afa59412bfec9311555db0ae
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-21 01:49:24 +0200

    include router and remove logging config

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
-import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.monitor_router import monitor_router
 from routers.probe_router import probe_router
+from routers.arp_spoof_router import arp_spoof_router
 from routers.port_scan_router import port_scan_router
 from routers.os_detection_router import os_detection_router
 from routers.event_router import event_router
 
 app = FastAPI()
-
-logging.basicConfig(level=logging.INFO)
 
 origins = [
     "http://localhost:5173",
@@ -25,6 +23,7 @@ app.add_middleware(
 
 app.include_router(monitor_router, prefix="/monitor")
 app.include_router(probe_router, prefix="/probe")
+app.include_router(arp_spoof_router, prefix="/arp-spoof")
 app.include_router(event_router, prefix="/events")
 app.include_router(port_scan_router)
 app.include_router(os_detection_router)
